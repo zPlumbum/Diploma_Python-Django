be0d38757f6b0b4d1312129d9e1e2a51179e51c3
Alexander Kikot <academic77@yandex.ru>
2021-04-17 15:49:12 +0300

    Modified REST_FRAMEWORK.

--- django_diploma/settings.py
+++ django_diploma/settings.py
@@ -80,6 +80,12 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
     ]
 }
 
