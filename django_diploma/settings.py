3807f4bd30baca0387d937d13c3433cbe6bf73dd
Alexander Kikot <academic77@yandex.ru>
2021-04-17 16:32:39 +0300

    Prepared for Heroku.

--- django_diploma/settings.py
+++ django_diploma/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,3 +147,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.User'
+
+django_heroku.settings(locals())
