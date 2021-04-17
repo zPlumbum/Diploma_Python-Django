4ad25e75be3e0828256ed0185fd5de24aba1487b
Alexander Kikot <academic77@yandex.ru>
2021-04-17 15:36:23 +0300

    ci.yml has been added.

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 from user.models import User
 from user.serializers import UserSerializer
